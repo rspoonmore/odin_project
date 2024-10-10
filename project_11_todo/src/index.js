0b3c0b178ee00455e0189a8c911927be991199e2
rspoonmore <rspoonmore@gmail.com>
2024-10-10 15:31:21 -0500

    Start work on js files

--- project_11_todo/src/index.js
+++ project_11_todo/src/index.js
@@ -1,1 +1,16 @@
 import "./style.css";
+import {create_category} from "./categories.js"
+import {addCategoryToHTML} from "./html_editor.js"
+
+let testButton = document.createElement('button');
+testButton.innerText = 'Add Test';
+testButton.addEventListener('click', () => {
+    let cat = create_category('Tomorrow');
+    cat.addTask('Wrap Presents', 'Wrap the birthday presents for Carson.', Date(2024, 10, 15), '1');
+    console.log(cat);
+    addCategoryToHTML(cat);
+});
+
+
+document.querySelector('body').appendChild(testButton);
+
