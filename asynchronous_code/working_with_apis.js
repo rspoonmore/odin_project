ec943f4f3335a2b314d9acff93190c583bbb8875
rspoonmore <rspoonmore@gmail.com>
2025-02-04 19:32:03 -0600

    Finish GIt Search Exercise

--- asynchronous_code/working_with_apis.js
+++ asynchronous_code/working_with_apis.js
@@ -1,26 +1,27 @@
+const form = document.querySelector('form');
 const img = document.querySelector('img');
-const searchBar = document.querySelector('#searchBar')
+const searchBar = document.querySelector('#searchBar');
 const button = document.querySelector('button');
-apiKey = 'P8cexa5XvLlupQQGytiyVt2TjslDepEM'
+const apiKey = 'P8cexa5XvLlupQQGytiyVt2TjslDepEM';
 
 function searchGIFS(searchField) {
     const url = `https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${searchField}`;
     fetch(url, {mode: 'cors'})
     .then(response => {
-        return response.json();
+        return response.json()
     })
-    .then(response => {
-        return response.data.images.original.url;
+    .then(newResponse => {
+        img.src = newResponse.data.images.original.url
     })
     .catch(err => {
         console.log(err)
     })
-}
+};
 
-button.addEventListener('click', () => {
-    const gifURL = searchGIFS(searchBar.value);
-    img.src = gifURL;
-})
-
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log(searchBar.value);
+    searchGIFS(searchBar.value);
+});
 
 
