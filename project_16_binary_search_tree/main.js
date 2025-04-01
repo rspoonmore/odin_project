2d3dbf9837b80b797d1702617d1306989cf57672
rspoonmore <rspoonmore@gmail.com>
2025-04-01 08:42:43 -0500

    Finish binary tree project

--- project_16_binary_search_tree/main.js
+++ project_16_binary_search_tree/main.js
@@ -1,34 +1,56 @@
 import { Tree } from "./bst.js";
 
+let randArray = []
+for(let i = 0; i < 10; i++) {
+    randArray.push(Math.floor(Math.random() * 100))
+}
+const tree = new Tree(randArray);
 
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 tree.print()
+console.log(`isBalanced: ${tree.isBalanced()}`)
+console.log('*************************************************')
+console.log('print in level')
+tree.levelOrder((n) => {console.log(n.data)})
 
-// console.log('************************************************************')
-// console.log('Remove 67')
-// tree.remove(67);
-// tree.print();
+console.log('*************************************************')
+console.log('print pre')
+tree.preOrder((n) => {console.log(n.data)})
 
-// console.log('************************************************************')
-// console.log('Add 25')
-// tree.insert(25);
-// tree.print();
+console.log('*************************************************')
+console.log('print post')
+tree.postOrder((n) => {console.log(n.data)})
 
-// console.log('************************************************************')
-// console.log('Find 6345')
-// console.log(tree.find(6345))
+console.log('*************************************************')
+console.log('print in order')
+tree.inOrder((n) => {console.log(n.data)})
 
-// console.log('************************************************************')
-// console.log('Find 45')
-// console.log(tree.find(45))
+console.log('*************************************************')
+console.log('Add 3 numbers > 100')
+for(let i = 0; i < 3; i++) {
+    let newNum = Math.floor(Math.random() * 100 + 101)
+    tree.insert(newNum)
+}
+tree.print()
+console.log(`isBalanced: ${tree.isBalanced()}`)
 
+console.log('*************************************************')
+console.log('Rebalance Tree')
+tree.rebalance()
+tree.print()
+console.log(`isBalanced: ${tree.isBalanced()}`)
 
-console.log('************************************************************')
-console.log('postOrder console.log')
-let f = (n) => {console.log(n.data)};
-tree.postOrder(f)
+console.log('*************************************************')
+console.log('print in level')
+tree.levelOrder((n) => {console.log(n.data)})
 
+console.log('*************************************************')
+console.log('print pre')
+tree.preOrder((n) => {console.log(n.data)})
 
-console.log('************************************************************')
-console.log('depth of first 9')
-console.log(tree.depth(tree.root.right.left))
+console.log('*************************************************')
+console.log('print post')
+tree.postOrder((n) => {console.log(n.data)})
+
+console.log('*************************************************')
+console.log('print in order')
+tree.inOrder((n) => {console.log(n.data)})
