d21e132e3925f5fab0440d91566395a2d2b926f2
rspoonmore <rspoonmore@gmail.com>
2025-04-08 13:35:04 -0500

    Finish project 18

--- project_18_js_testing_practice/main.js
+++ project_18_js_testing_practice/main.js
@@ -2,4 +2,78 @@ function capitalize(s) {
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
-module.exports = {'capitalize': capitalize};
+function reverseString(s) {
+    let res = '';
+    const sArray = s.split('');
+    for(let i = sArray.length - 1; i >= 0; i--){
+        res += sArray[i]
+    }
+    return res;
+}
+
+function calculator(operation, a, b){
+    if(operation == 'add') {return a + b}
+    if(operation == 'subtract') {return a - b}
+    if(operation == 'divide') {return a / b}
+    if(operation == 'multiply') {return a * b}
+    return undefined;
+}
+
+function shiftLetterBy(letter, shiftAmount) {
+    const letterCode = letter.charCodeAt();
+    if(letterCode < 65 | letterCode > 122 | (letterCode > 90 & letterCode < 97)) {return letter};
+    let newLetterCode = letterCode + shiftAmount;
+    const upperRange = (letterCode >= 65 & letterCode <= 90) ? 90 : 122;
+    const lowerRange = (letterCode >= 65 & letterCode <= 90) ? 65 : 97;
+    if(newLetterCode > upperRange){
+        newLetterCode = lowerRange + newLetterCode - upperRange - 1;
+    }
+    else if(newLetterCode < lowerRange) {
+        newLetterCode = upperRange + newLetterCode - lowerRange + 1;
+    }
+    return String.fromCharCode(newLetterCode);
+}
+
+function caesarCipher(word, shift) {
+    let result = "";
+    const wordArray = word.split('');
+    for(let i = 0; i < wordArray.length; i++) {
+        result += shiftLetterBy(wordArray[i], shift)
+    }
+    return result;
+}
+
+function isNumber(value) {
+    return /^-?\d+(\.\d+)?$/.test(value);
+  }
+
+function analyzeArray(arr) {
+    const lArr = arr.length;
+    let _min = undefined;
+    let _max = undefined;
+    let _sum = 0;
+    let lArrTrue = 0;
+    for(let i = 0; i < lArr; i++){
+        const currentNum = arr[i]
+        if(isNumber(currentNum)){
+            lArrTrue += 1;
+            _sum += currentNum;
+            if(currentNum > _max | _max == undefined) {_max = currentNum};
+            if(currentNum < _min | _min == undefined) {_min = currentNum};
+        }
+    }
+    return {
+        'average': _sum / lArrTrue,
+        'min': _min,
+        'max': _max,
+        'length': lArr
+    }
+}
+
+module.exports = {
+    'capitalize': capitalize, 
+    'reverseString': reverseString,
+    'calculator': calculator,
+    'caesarCipher': caesarCipher,
+    'analyzeArray': analyzeArray
+};
--- project_18_js_testing_practice/main.test.js
+++ project_18_js_testing_practice/main.test.js
@@ -12,3 +12,63 @@ test('Test capitalize', () => {
         }
     })
 })
+
+test('Reverse String', () => {
+    const testWords = ['lkLKJAIlwke', 'test', 'MMM', 'iI'];
+    testWords.forEach((word) => {
+        const wordArray = word.split('');
+        const resultArray = ex['reverseString'](word).split('');
+        expect(resultArray.length).toBe(wordArray.length);
+        for(let i = 0; i < wordArray.length; i++){
+            expect(wordArray[wordArray.length - i - 1]).toBe(resultArray[i])
+        }
+    })
+})
+
+test('Calculator', () => {
+    for(let i = 0; i < 10; i++){
+        const a = Math.random() * 200 - 100;
+        const b = Math.random() * 200 - 100;
+        expect(ex['calculator']('add', a, b)).toBeCloseTo(a + b);
+        expect(ex['calculator']('subtract', a, b)).toBeCloseTo(a - b);
+        expect(ex['calculator']('divide', a, b)).toBeCloseTo(a / b);
+        expect(ex['calculator']('multiply', a, b)).toBeCloseTo(a * b);
+        expect(ex['calculator']('test', a, b)).toBeUndefined();
+    }
+})
+
+test('Caesar Cipher', () => {
+    const testWords = {
+        'Hello': [2, 'Jgnnq'], 
+        'A': [-2, 'Y'],
+        'a': [-3, 'x'],
+        'y': [4, 'c'],
+        'Y': [2, 'A'],
+        'He,!-0llo': [1, 'If,!-0mmp']
+    }
+    Object.entries(testWords).forEach(([key, value]) => {
+        expect(ex['caesarCipher'](key, value[0])).toBe(value[1])
+    })
+})
+
+test('Analyze Array', () => {
+    const arraySize = 15;
+    for(let i = 0; i < 10; i++){
+        let testArray = [];
+        for(let j = 0; j < arraySize; j++){
+            testArray.push(Math.random() * 200 - 100)
+        }
+        const testAnswer = ex['analyzeArray'](testArray);
+        expect(testAnswer['average']).toBeCloseTo(testArray.reduce((a, b) => a + b) / testArray.length);
+        expect(testAnswer['min']).toBeCloseTo(Math.min(...testArray));
+        expect(testAnswer['max']).toBeCloseTo(Math.max(...testArray));
+        expect(testAnswer['length']).toBeCloseTo(testAnswer.length);
+    }
+
+    const testArray = [1, 2, undefined, 4];
+    const testAnswer = ex['analyzeArray'](testArray);
+    expect(testAnswer['average']).toBeCloseTo(7/3);
+    expect(testAnswer['min']).toBeCloseTo(1);
+    expect(testAnswer['max']).toBeCloseTo(4);
+    expect(testAnswer['length']).toBeCloseTo(4);
+})
