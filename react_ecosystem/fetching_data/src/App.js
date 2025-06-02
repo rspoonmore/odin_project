3e414f9a1c031717f81d6e54f0eedc4375265291
rspoonmore <rspoonmore@gmail.com>
2025-06-02 08:57:41 -0500

    Complete fetching data in React module

---
+++ react_ecosystem/fetching_data/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Image from './Image';
+
+function App() {
+  return (
+    <Image />
+  )
+}
+
+export default App;
---
+++ react_ecosystem/fetching_data/src/Image.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+const Image = () => {
+  const [imageURL, setImageURL] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/charizard", { mode: "cors" })
+      .then((response) => {
+        if (response.status >= 400) {
+            throw new Error("server error");
+        }
+        return response.json()
+      })
+      .then((response) => setImageURL(response['sprites']['front_default']))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>A netwrok error was encountered</p>
+
+  return (
+    imageURL && (
+      <>
+        <h1>An image</h1>
+        <img src={imageURL} alt={"placeholder text"} />
+      </>
+    )
+  );
+};
+
+export default Image;
